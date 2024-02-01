76de6635756229adbb8625fc2b3b623742aaeba5
rupinder <ello.rupinder@gmail.com>
2024-02-01 18:57:20 +0530

    feat: add bus images

--- src/lib/components/section/Hero.svelte
+++ src/lib/components/section/Hero.svelte
@@ -8,14 +8,14 @@
 		<div
 			class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-6 lg:mb-16 md:mb-0 items-center text-center"
 		>
-			<h1 class="lg:text-5xl leading-10 text-3xl pb-4 font-medium ">
-				<span class="text-primary font-bold">Tata Bus Service</span> - Travel Safe with Us. Your safety Our
-				Priority
+			<h1 class="lg:text-5xl leading-10 text-3xl pb-4 font-medium">
+				<span class="text-primary font-bold">Tata Bus Service</span> - Travel Safe with Us. Your safety
+				Our Priority
 			</h1>
 			<p class="text-xs md:text-s mb-8 leading-relaxed">
-				Tata Bus stands as a prominent force in the bus rental industry in Punjab. Our commitment
-				to enhancing the travel experience is evident through the continuous upgrades to our
-				extensive fleet, ensuring our customers enjoy a journey marked by comfort and convenience.
+				Tata Bus stands as a prominent force in the bus rental industry in Punjab. Our commitment to
+				enhancing the travel experience is evident through the continuous upgrades to our extensive
+				fleet, ensuring our customers enjoy a journey marked by comfort and convenience.
 			</p>
 			<p class="text-xs md:text-s mb-8 leading-relaxed">
 				With a rich legacy spanning over four decades, Dhillon Motors exemplifies a steadfast
