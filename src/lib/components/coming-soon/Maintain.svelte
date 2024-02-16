09e0b4a23620e71360f7ec9f3350cdc4d744fd12
rupinder <ello.rupinder@gmail.com>
2024-02-16 11:07:05 +0530

    add maintaince image

---
+++ src/lib/components/coming-soon/Maintain.svelte
@@ -0,0 +1,24 @@
+<script>
+	// import { goto } from '$app/navigation';
+	import maintain from '$lib/images/maintain.jpg';
+	// import Carousel from '../carousel/Carousel.svelte';
+	// import Button from '../ui/Button.svelte';
+
+	// const onClick = () => goto('/contact');
+</script>
+
+<!-- <section class="px-4 lg:px-14 xl:px-18 h-full  bg-map bg-no-repeat bg-unset md:bg-right"> -->
+<div class="centered-image">
+	<img class="flex items-center justify-center mx-auto" src={maintain} alt="maintaince" />
+</div>
+
+<!-- </section> -->
+
+<style>
+	.centered-image {
+		display: flex;
+		justify-content: center; /* Center horizontally */
+		align-items: center; /* Center vertically */
+		height: 100vh; /* Adjust the height as needed */
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import Navbar from '$lib/components/NavBar/Navbar.svelte';
 	import Topbar from '$lib/components/NavBar/Topbar.svelte';
-	// import ComingSoon from '$lib/components/coming-soon/ComingSoon.svelte';
+	import ComingSoon from '$lib/components/coming-soon/ComingSoon.svelte';
+	import Maintain from '$lib/components/coming-soon/Maintain.svelte';
 	import Footer from '$lib/components/footer/Footer.svelte';
 
 	// import Header from './Header.svelte';
 	import './styles.css';
 </script>
 
-<Topbar />
-<Navbar />
-<!-- <ComingSoon /> -->
-<main class="max-w-[1920px] mx-auto">
+<!-- <Topbar /> -->
+<!-- <Navbar /> -->
+<Maintain />
+<!-- <main class="max-w-[1920px] mx-auto">
 	<slot />
-</main>
-<Footer />
+</main> -->
+<!-- <Footer /> -->
